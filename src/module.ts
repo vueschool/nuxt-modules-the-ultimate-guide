296f2acf96f3c00b8bf76b12c6c574bc4caf1d09
Mostafa Said <mostafasaid1994@gmail.com>
2024-01-29 19:20:26 +0200

    feat: :sparkles: lesson-11-injecting-plugin-templates

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,13 @@
-import { defineNuxtModule, addPlugin, createResolver } from "@nuxt/kit";
+import {
+  defineNuxtModule,
+  addPlugin,
+  addPluginTemplate,
+  createResolver,
+} from "@nuxt/kit";
 
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  activateObserver: boolean;
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -8,14 +15,40 @@ export default defineNuxtModule<ModuleOptions>({
     configKey: "myModule",
   },
 
-  defaults: {},
+  defaults: {
+    activateObserver: true,
+  },
   setup(options, nuxt) {
     const { resolve } = createResolver(import.meta.url);
 
-    addPlugin({
-      src: resolve("./runtime/observer"),
+    const clientPlugin = options.activateObserver
+      ? `import { defineNuxtPlugin } from "#imports";
+
+export default defineNuxtPlugin(async (nuxtApp) => {
+
+const VueObserveVisibility = await import("vue3-observe-visibility").then(
+(m) => m.default
+);
+
+nuxtApp.vueApp.use(VueObserveVisibility);
+
+});`
+      : `import { defineNuxtPlugin } from "#imports";
+
+export default defineNuxtPlugin(async (nuxtApp) => {
+
+nuxtApp.vueApp.directive("observe-visibility", {});
+
+});`;
+
+    addPluginTemplate({
+      getContents: () => clientPlugin,
+      filename: "observer.mjs",
       mode: "client",
+      write: true,
+      dst: resolve("./runtime/observer.ts"),
     });
+
     addPlugin({
       src: resolve("./runtime/observer-stub"),
       mode: "server",
--- src/runtime/observer.ts
+++ src/runtime/observer.ts
@@ -1,6 +1,7 @@
 import { defineNuxtPlugin } from "#imports";
-import VueObserveVisibility from "vue3-observe-visibility";
 
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueObserveVisibility, {});
+export default defineNuxtPlugin(async (nuxtApp) => {
+
+nuxtApp.vueApp.directive("observe-visibility", {});
+
 });
